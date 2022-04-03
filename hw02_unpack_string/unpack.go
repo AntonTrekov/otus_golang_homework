ff706364d043197fc51646c80c905de35a151777
Anton Dorozhkin <anton.dorozhkin@lamoda.ru>
2022-04-03 03:22:57 +0300

    hw02 Unpacking strings

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,57 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var (
+		result      strings.Builder
+		buffer      strings.Builder
+		resultError error
+	)
+
+	for _, charRune := range input {
+		if buffer.Len() == 0 && unicode.IsLetter(charRune) {
+			buffer.WriteRune(charRune)
+
+			continue
+		}
+
+		if buffer.Len() == 1 && unicode.IsLetter(charRune) {
+			result.WriteString(buffer.String())
+			buffer.Reset()
+			buffer.WriteRune(charRune)
+
+			continue
+		}
+
+		if buffer.Len() == 1 && unicode.IsDigit(charRune) {
+			count, err := strconv.Atoi(string([]rune{charRune}))
+
+			if err != nil {
+				return "", err
+			}
+
+			result.WriteString(
+				strings.Repeat(buffer.String(), count),
+			)
+			buffer.Reset()
+
+			continue
+		}
+
+		resultError = ErrInvalidString
+		result.Reset()
+	}
+
+	if resultError == nil {
+		result.WriteString(buffer.String())
+	}
+
+	return result.String(), resultError
 }
