bf1a67614b9c989e8d51d48eb991d4cac10d74be
Anton Trekov <cheech47@yandex.ru>
2022-04-04 09:54:56 +0300

    hw02 Unpacking strings - symbol escaping

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -5,27 +5,51 @@ import (
 	"strconv"
 	"strings"
 	"unicode"
+	"unicode/utf8"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var (
+	ErrInvalidString = errors.New("invalid string")
+	backslash        = `\`
+	backslashRune, _ = utf8.DecodeRuneInString(backslash)
+)
 
 func Unpack(input string) (string, error) {
 	var (
-		result      strings.Builder
-		buffer      strings.Builder
-		resultError error
+		result            strings.Builder
+		buffer            strings.Builder
+		resultError       error
+		isEscapingEnabled = false
 	)
 
 	for _, charRune := range input {
-		if buffer.Len() == 0 && unicode.IsLetter(charRune) {
-			buffer.WriteRune(charRune)
+		if buffer.Len() == 0 && charRune == backslashRune && !isEscapingEnabled {
+			isEscapingEnabled = true
 
 			continue
 		}
 
-		if buffer.Len() == 1 && unicode.IsLetter(charRune) {
+		isCurrentSymbolEscaped := isEscapingEnabled && (unicode.IsDigit(charRune) || charRune == backslashRune)
+
+		if buffer.Len() == 0 && (unicode.IsLetter(charRune) || isCurrentSymbolEscaped) {
+			buffer.WriteRune(charRune)
+			isEscapingEnabled = false
+
+			continue
+		}
+
+		if buffer.Len() == 1 && charRune == backslashRune {
 			result.WriteString(buffer.String())
 			buffer.Reset()
+			isEscapingEnabled = true
+
+			continue
+		}
+
+		if buffer.Len() == 1 && (unicode.IsLetter(charRune) || isCurrentSymbolEscaped) {
+			result.WriteString(buffer.String())
+			buffer.Reset()
+			isEscapingEnabled = false
 			buffer.WriteRune(charRune)
 
 			continue
@@ -33,7 +57,6 @@ func Unpack(input string) (string, error) {
 
 		if buffer.Len() == 1 && unicode.IsDigit(charRune) {
 			count, err := strconv.Atoi(string([]rune{charRune}))
-
 			if err != nil {
 				return "", err
 			}
@@ -42,6 +65,7 @@ func Unpack(input string) (string, error) {
 				strings.Repeat(buffer.String(), count),
 			)
 			buffer.Reset()
+			isEscapingEnabled = false
 
 			continue
 		}
